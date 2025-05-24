36d0445f2c8a31ec1174d1fb138a2e0a26eae8db
getlarge <ed@getlarge.eu>
2025-05-24 20:51:18 +0200

    chore(purrfect-sitter-e2e): enable dependency checks

--- apps/purrfect-sitter-e2e/eslint.config.mjs
+++ apps/purrfect-sitter-e2e/eslint.config.mjs
@@ -1,3 +1,26 @@
 import baseConfig from '../../eslint.config.mjs';
 
-export default [...baseConfig];
+export default [
+  ...baseConfig,
+  {
+    files: ['**/*.json'],
+    rules: {
+      '@nx/dependency-checks': [
+        'error',
+        {
+          buildTargets: ['e2e'],
+          ignoredFiles: ['{projectRoot}/eslint.config.{js,cjs,mjs}'],
+          checkMissingDependencies: true,
+          checkObsoleteDependencies: true,
+          checkVersionMismatches: true,
+          ignoredFiles: ['eslint.config.mjs'],
+          includeTransitiveDependencies: true,
+          useLocalPathsForWorkspaceDependencies: true,
+        },
+      ],
+    },
+    languageOptions: {
+      parser: await import('jsonc-eslint-parser'),
+    },
+  },
+];
